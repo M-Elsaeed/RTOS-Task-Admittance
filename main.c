a2ef7f8a92f37f224fdf62d7eddd60be3084db7b
M-Elsaeed <m.elsaeed1998@gmail.com>
2020-05-14 15:27:13 +0200

    creation of rtos tasks, refactoring

--- main.c
+++ main.c
@@ -16,32 +16,31 @@ int n = 5;
 #define TST 1
 #define SAFE_MODE 0
 
-typedef struct Tasks
+typedef struct Task
 {
+	int id;
 	double arrival;
-	double period;
+	double period; //Safe: random from 3xTc(i) to maximum_period_multipler x Tc(i) | No Guarantee: random from from 3xTc(i) to 10xTc(i)
 	double computation; // random from 1 to maximum_computation_time
-						/* 
-Safe 			: random from 3xTc(i) to maximum_period_multipler x Tc(i)
-No Guarantee 	: random from from 3xTc(i) to 10xTc(i)
-*/
 	int priority;
-} Tasks;
+} Task;
 
 static int myCompare(const void *a, const void *b)
 {
 
-	const Tasks *t1 = (Tasks *)a;
-	const Tasks *t2 = (Tasks *)b;
+	const Task *t1 = (Task *)a;
+	const Task *t2 = (Task *)b;
 	// setting up rules for comparison
 	return t1->period < t2->period;
 }
 
 // Function to sortAndPrioritize the array
-void sortAndPrioritize(Tasks arr[], int n)
+void sortAndPrioritize(Task arr[], int n)
 {
 	int i = 0, lastPriority = 1;
-	qsort(arr, n, sizeof(Tasks), myCompare);
+	qsort(arr, n, sizeof(Task), myCompare);
+
+
 	for (i = 0; i < n - 1; i++)
 	{
 		if (arr[i].period != arr[i + 1].period)
@@ -52,7 +51,7 @@ void sortAndPrioritize(Tasks arr[], int n)
 	arr[i].priority = lastPriority;
 }
 
-double computeUtilization(Tasks a_tasksArr[], int a_size)
+double computeUtilization(Task a_tasksArr[], int a_size)
 {
 	int i = 0;
 	double cpuUtilization = 0.0;
@@ -65,23 +64,23 @@ double computeUtilization(Tasks a_tasksArr[], int a_size)
 	return cpuUtilization;
 }
 
-void printTasks(Tasks a_tasksArr[], int a_size)
+void printTasks(Task a_tasksArr[], int a_size)
 {
 	int i = 0;
 	printf("Print called\n");
 	for (i = 0; i < a_size; i++)
 	{
-		printf("i: %d, c: %lf, p: %lf \n", a_tasksArr[i].priority, a_tasksArr[i].computation, a_tasksArr[i].period);
+		printf("id:%d pri: %d, com: %lf, per: %lf \n", a_tasksArr[i].id, a_tasksArr[i].priority, a_tasksArr[i].computation, a_tasksArr[i].period);
 	}
 }
 
 int currTasks = 0;
-Tasks *arrayOfTasks;
+Task *arrayOfTasks;
 
-void admitTask(Tasks newTask)
+void admitTask(Task newTask)
 {
 	int i = 0;
-	Tasks *temp = malloc((currTasks + 1) * sizeof(Tasks));
+	Task *temp = malloc((currTasks + 1) * sizeof(Task));
 
 	if (!temp)
 		printf("allocNO\n");
@@ -98,10 +97,11 @@ void admitTask(Tasks newTask)
 	arrayOfTasks[currTasks++] = newTask;
 	sortAndPrioritize(arrayOfTasks, currTasks);
 }
+
 void deleteTask(int index)
 {
 	int i = 0;
-	Tasks *temp = malloc((currTasks - 1) * sizeof(Tasks));
+	Task *temp = malloc((currTasks - 1) * sizeof(Task));
 
 	if (!temp)
 		printf("allocNO\n");
@@ -114,58 +114,72 @@ void deleteTask(int index)
 	}
 	for (i = index + 1; i < currTasks; i++)
 	{
-		temp[i-1] = arrayOfTasks[i];
+		temp[i - 1] = arrayOfTasks[i];
 	}
 
 	free(arrayOfTasks);
 	arrayOfTasks = temp;
-	currTasks --;
+	currTasks--;
 	sortAndPrioritize(arrayOfTasks, currTasks);
 }
+
 int main(void)
 {
-	Tasks tempTask = {3, 11, 2};
+	Task tempTask = {1, 3, 11, 2};
 	admitTask(tempTask);
 
+	tempTask.id = 2;
 	tempTask.arrival = 9;
 	tempTask.period = 20;
 	tempTask.computation = 1;
 	admitTask(tempTask);
 
+	tempTask.id = 3;
 	tempTask.arrival = 12;
 	tempTask.period = 32;
 	tempTask.computation = 5;
 	admitTask(tempTask);
 
+	tempTask.id = 4;
 	tempTask.arrival = 6;
 	tempTask.period = 27;
 	tempTask.computation = 4;
 	admitTask(tempTask);
 
+	tempTask.id = 5;
 	tempTask.arrival = 13;
 	tempTask.period = 13;
 	tempTask.computation = 1;
 	admitTask(tempTask);
 
-	printTasks(arrayOfTasks, currTasks);
-	deleteTask(2);
+	tempTask.id = 5;
+	tempTask.arrival = 13;
+	tempTask.period = 13;
+	tempTask.computation = 1;
+	admitTask(tempTask);
+	// printTasks(arrayOfTasks, currTasks);
+	// deleteTask(2);
 	printTasks(arrayOfTasks, currTasks);
 	// sortAndPrioritize(arrayOfTasks, currTasks);
 	/* Create one of the two tasks. */
-	//	xTaskCreate(vTaskFunction,			/* Pointer to the function that implements the task. */
-	//				"Task 1",				/* Text name for the task.  This is to facilitate debugging only. */
-	//				240,					/* Stack depth in words. */
-	//				(void *)pcTextForTask1, /* Pass the text to be printed in as the task parameter. */
-	//				1,						/* This task will run at priority 1. */
-	//				NULL);					/* We are not using the task handle. */
+	// xTaskCreate(vTaskFunction,			/* Pointer to the function that implements the task. */
+	// 			"Task 1",				/* Text name for the task.  This is to facilitate debugging only. */
+	// 			128,					/* Stack depth in words. */
+	// 			(void *)&arrayOfTasks[0], /* Pass the text to be printed in as the task parameter. */
+	// 			1,						/* This task will run at priority 1. */
+	// 			NULL);					/* We are not using the task handle. */
 
 	/* Create the other task in exactly the same way.  Note this time that we
 	are creating the SAME task, but passing in a different parameter.  We are
 	creating two instances of a single task implementation. */
-	//	xTaskCreate(vTaskFunction, "Task 2", 240, (void *)pcTextForTask2, 1, NULL);
-
+	xTaskCreate(vTaskFunction, "Task 1", 100, (void *)&arrayOfTasks[0], 1, NULL);
+	xTaskCreate(vTaskFunction, "Task 2", 100, (void *)&arrayOfTasks[1], 1, NULL);
+	xTaskCreate(vTaskFunction, "Task 3", 100, (void *)&arrayOfTasks[2], 1, NULL);
+	xTaskCreate(vTaskFunction, "Task 4", 100, (void *)&arrayOfTasks[3], 1, NULL);
+	xTaskCreate(vTaskFunction, "Task 5", 100, (void *)&arrayOfTasks[4], 1, NULL);
+	xTaskCreate(vTaskFunction, "Task 6", 100, (void *)&arrayOfTasks[5], 1, NULL);
 	/* Start the scheduler so our tasks start executing. */
-	//	vTaskStartScheduler();
+	vTaskStartScheduler();
 
 	/* If all is well we will never reach here as the scheduler will now be
 	running.  If we do reach here then it is likely that there was insufficient
@@ -175,30 +189,16 @@ int main(void)
 }
 /*-----------------------------------------------------------*/
 
-//void vTaskFunction(void *pvParameters)
-//{
-//	char *pcTaskName;
-//	volatile unsigned long ul;
+void vTaskFunction(void *pvParameters)
+{
+	Task t = *((Task *)(pvParameters));
 
-//	/* The string to print out is passed in via the parameter.  Cast this to a
-//	character pointer. */
-//	pcTaskName = (char *)pvParameters;
-
-//	/* As per most tasks, this task is implemented in an infinite loop. */
-//	for (;;)
-//	{
-//		/* Print out the name of this task. */
-//		vPrintString(pcTaskName);
-
-//		/* Delay for a period. */
-//		for (ul = 0; ul < mainDELAY_LOOP_COUNT; ul++)
-//		{
-//			/* This loop is just a very crude delay implementation.  There is
-//			nothing to do in here.  Later exercises will replace this crude
-//			loop with a proper delay/sleep function. */
-//		}
-//	}
-//}
+	/* As per most tasks, this task is implemented in an infinite loop. */
+	for (;;)
+	{
+		printf("%d\n", t.id);
+	}
+}
 // /*-----------------------------------------------------------*/
 
 // void vApplicationMallocFailedHook(void)
